800add00227916806c9779b9e0e851f846b89d0c
Esneider Herrera <reyalejandroh@gmail.com>
2022-09-15 19:21:10 +0300

    Added button in constants

--- src/assets/Pages/Login/index.tsx
+++ src/assets/Pages/Login/index.tsx
@@ -22,10 +22,10 @@ function closePopUp(){
             <div className="login-content">
                 <img src={logoBasketFeature} alt="basket-feature" id="logo-basket-feature"/>
                 <form action="" id="login-form">
-                    <input type="email" name="email" id="email-login" placeholder="Электронная почта" required/>
-                    <input type="password" name="password" id="password-login" placeholder="Пароль" required/>
+                    <input type="email" name="email" id="email-login" className="input-basic" placeholder="Электронная почта" required/>
+                    <input type="password" name="password" id="password-login" className="input-basic" placeholder="Пароль" required/>
                     <p id="reset-password-link" onClick={openPopUp}>забыл пароль</p>
-                    <input id="input-submit-login" type="submit" value="Войти" />
+                    <input id="input-submit" className="input-basic" type="submit" value="Войти" />
                     <NavLink id="signup-link" to="/sign-up">зарегистрироваться</NavLink>
                 </form>
                 <PopUpResetPassword display={displayPopUp} onClose={closePopUp}/>
--- src/assets/Pages/SignUp/index.tsx
+++ src/assets/Pages/SignUp/index.tsx
@@ -7,11 +7,11 @@ export default function SignUp(){
                 <div className="signup-content">
                     <form action="" id="form-signup">
                         <h2 className="title-page">Регистрация</h2>
-                        <input type="email" name="email" id="email-signup" placeholder="Электронная почта" required/>
+                        <input type="email" name="email" id="email-signup" className="input-basic" placeholder="Электронная почта" required/>
                         <label id="label-password" htmlFor="password"> Пароль должен содержать минимум 8 символов латинскими буквами, а также хотя бы одну цифру</label>
-                        <input type="password" name="password" id="password-signup" placeholder="Пароль" required/>
-                        <input type="password" name="repassword" id="repassword-signup" placeholder="Повторите пароль" required/>
-                        <input type="submit" id="input-submit" value="Зарегистрироваться" />
+                        <input type="password" name="password" id="password-signup" className="input-basic" placeholder="Пароль" required/>
+                        <input type="password" name="repassword" id="repassword-signup" className="input-basic" placeholder="Повторите пароль" required/>
+                        <input type="submit" id="input-submit" className="input-basic" value="Зарегистрироваться" />
                         <NavLink id="forgot-password" to="/">уже есть аккаунт</NavLink>
                     </form>
                 </div>
