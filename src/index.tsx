92266c7364e5442cea2bd0fadbbe99e0b0d65362
Esneider Herrera <reyalejandroh@gmail.com>
2024-06-17 23:23:49 +0300

    solved

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+const rootNode = document.getElementById("root");
+
+ReactDOM.render(
   <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </Router>
+  </Router>,
+  rootNode
 );
 
 // If you want to start measuring performance in your app, pass a function
